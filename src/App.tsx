f5985cf8329f6990140bb1d637fbe6172bfd0169
Andrea Pedreño <apedrenodiez@gmail.com>
2022-11-25 16:41:17 +0100

    Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import AppStyled from "./AppStyled";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import Header from "./components/Header/Header";
 
 const App = () => {
+  const { pathname } = useLocation();
+
   return (
     <AppStyled>
+      {"/" !== pathname && <Header />}
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/register" element={<RegisterPage />} />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import HeaderStyled from "./HeaderStyled";
+import { ReactComponent as Logo } from "../../assets/blend-logo.svg";
+
+const Header = (): JSX.Element => {
+  return (
+    <HeaderStyled className="header">
+      <Link to="/">
+        <Logo
+          className="welcome-page__logo"
+          aria-label="A cup of tea with the text blend"
+          width="360"
+        />
+      </Link>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
