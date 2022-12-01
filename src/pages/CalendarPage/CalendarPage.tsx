25e7b546d0dee3ddd89d086fc613c721b1794b28
Andrea Pedreño <apedrenodiez@gmail.com>
2022-12-01 16:10:39 +0100

    Include app test and snapshot

--- src/pages/CalendarPage/CalendarPage.tsx
+++ src/pages/CalendarPage/CalendarPage.tsx
@@ -1,4 +1,4 @@
-import CalendarDay from "../../components/Days/CalendarDay";
+import CalendarDay from "../../components/CalendarDays/CalendarDay";
 import CalendarPageStyled from "./CalendarPageStyled";
 
 const CalendarPage = (): JSX.Element => {
@@ -13,7 +13,7 @@ const CalendarPage = (): JSX.Element => {
       <h2>Let’s open today’s window!</h2>
       <div className="days">
         {calendarDays.map((day) => (
-          <CalendarDay calendarDay={day} />
+          <CalendarDay key={day} calendarDay={day} />
         ))}
       </div>
     </CalendarPageStyled>
