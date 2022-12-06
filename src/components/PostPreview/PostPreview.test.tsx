e9cd01eacc463ccaee482c35cef6a224dfcee8b0
Andrea Pedreño <apedrenodiez@gmail.com>
2022-12-06 23:54:34 +0100

    Include post preview test

---
+++ src/components/PostPreview/PostPreview.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { postPropsMock } from "../../mocks/listOfPostsMock";
+import { newStore } from "../../redux/store";
+import PostPreview from "./PostPreview";
+
+describe("Given a PostPreview component", () => {
+  describe("When it is rendered with a post title 'Pancake Stack'", () => {
+    test("Then it should show the text in a heading of level 3", async () => {
+      const postTest = postPropsMock;
+      const buttonText = "Delete";
+
+      render(
+        <BrowserRouter>
+          <Provider store={newStore}>
+            <PostPreview post={postTest} />
+          </Provider>
+        </BrowserRouter>
+      );
+      const post = screen.queryByRole("heading", { name: "Pancake Stack" });
+      const button = screen.queryAllByRole("button", { name: buttonText });
+
+      await userEvent.click(button[0]!);
+
+      expect(post).toBeInTheDocument();
+      expect(button[0]).toBeInTheDocument();
+    });
+  });
+});
