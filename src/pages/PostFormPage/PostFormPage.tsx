81f7330618022314abd81b16d12230d7c34bbff7
Andrea Pedreño <apedrenodiez@gmail.com>
2022-12-07 17:48:53 +0100

    Include cover at new post form card without data

--- src/pages/PostFormPage/PostFormPage.tsx
+++ src/pages/PostFormPage/PostFormPage.tsx
@@ -10,6 +10,7 @@ import Window from "../../components/Window/Window";
 import windows from "../../components/Window/windows";
 import { WindowStructure } from "../../types/calendarTypes";
 import { useAppSelector } from "../../redux/hooks";
+import { ReactComponent as NewPost } from "../../assets/new-post.svg";
 
 const PostFormPage = (): JSX.Element => {
   const { createPost, loadAllPosts } = useApi();
@@ -85,56 +86,54 @@ const PostFormPage = (): JSX.Element => {
   }, [loadAllPosts]);
 
   return (
-    <>
-      <Window {...windowData} />
-      <PostFormPageStyled onSubmit={handleSubmit}>
-        <SelectFormInput action={handleFormValues} windows={windowsData} />
-        <div className="input">
-          <span className="input__text">Have you added any ingredient?</span>
-          <SimpleFormInput
-            className="input__box"
-            name="answer1"
-            action={handleFormValues}
-          />
-        </div>
-        <div className="input">
-          <span className="input__text">Recommend a song for today’s tea:</span>
-          <SimpleFormInput
-            className="input__box"
-            name="answer2"
-            action={handleFormValues}
-          />
-        </div>
-        <div className="input">
-          <span className="input__text">Recommend a book for today’s tea:</span>
-          <SimpleFormInput
-            className="input__box"
-            name="answer3"
-            action={handleFormValues}
-          />
-        </div>
-        <div className="input">
-          <span className="input__text">How about your tea moment?</span>
-          <SimpleFormInput
-            className="input__box"
-            name="answer4"
-            action={handleFormValues}
-          />
-        </div>
-        <FormInput
-          placeholder="Add a picture"
-          type="text"
-          name="image"
+    <PostFormPageStyled onSubmit={handleSubmit}>
+      {!windowData?.title ? <NewPost /> : <Window {...windowData} />}
+      <SelectFormInput action={handleFormValues} windows={windowsData} />
+      <div className="input">
+        <span className="input__text">Have you added any ingredient?</span>
+        <SimpleFormInput
+          className="input__box"
+          name="answer1"
           action={handleFormValues}
         />
-        <Button
-          type="button"
-          className="form__button"
-          text="Send"
-          styletype="light"
+      </div>
+      <div className="input">
+        <span className="input__text">Recommend a song for today’s tea:</span>
+        <SimpleFormInput
+          className="input__box"
+          name="answer2"
+          action={handleFormValues}
         />
-      </PostFormPageStyled>
-    </>
+      </div>
+      <div className="input">
+        <span className="input__text">Recommend a book for today’s tea:</span>
+        <SimpleFormInput
+          className="input__box"
+          name="answer3"
+          action={handleFormValues}
+        />
+      </div>
+      <div className="input">
+        <span className="input__text">How about your tea moment?</span>
+        <SimpleFormInput
+          className="input__box"
+          name="answer4"
+          action={handleFormValues}
+        />
+      </div>
+      <FormInput
+        placeholder="Add a picture"
+        type="text"
+        name="image"
+        action={handleFormValues}
+      />
+      <Button
+        type="button"
+        className="form__button"
+        text="Send"
+        styletype="light"
+      />
+    </PostFormPageStyled>
   );
 };
 
